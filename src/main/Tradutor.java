f8e598b97c16abde37abad437b5e5cb2b299079d
Jorge Viegas <jorgematheusv@gmail.com>
2018-03-20 20:53:56 -0300

    Adding stream tokenizer

--- src/main/Tradutor.java
+++ src/main/Tradutor.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,7 +11,27 @@ public class Tradutor {
     static Pattern identifiers = Pattern.compile("([A-Za-z]([A-Za-z|[0-9]])*)");
 
     public static void main(String[] args) {
+
         Matcher lettersMatcher = letters.matcher("t");
         System.out.println(lettersMatcher.matches());
+
+        StringReader stringReader = new StringReader("string xalala void main 123");
+        StreamTokenizer streamTokenizer = new StreamTokenizer(stringReader);
+
+        boolean streamEOF = false;
+        try {
+            while(!streamEOF){
+
+                streamTokenizer.nextToken();
+                String currentToken = streamTokenizer.toString();
+                System.out.println("Token: " + currentToken);
+
+                if(streamTokenizer.ttype == -1) streamEOF = true;
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
