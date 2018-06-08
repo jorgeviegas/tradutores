f5c789c732290de133d3bb906df6fb32e9c2ac6f
Jorge Viegas <jorgematheusv@gmail.com>
2018-06-07 21:39:11 -0300

    Changing main method to call both lexical and syntatical analysis

--- src/main/SyntacticAnalyzer.java
+++ src/main/SyntacticAnalyzer.java
@@ -3,33 +3,32 @@ package main;
 import syntaticType.IReservedWord;
 import syntaticType.ISyntaticType;
 
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SyntacticAnalyzer {
 	private Lexeme currentLexeme;
+	private String currentToken;
 	private int currentIndex;
-
 	private LinkedList<Lexeme> lexemesList;
-
-	ISyntaticType a;
 	ISyntaticType exTokenType;
 
 	public SyntacticAnalyzer(LinkedList<Lexeme> lexemesList) {
 		this.lexemesList = lexemesList;
 		this.currentIndex = 0;
 		this.currentLexeme = lexemesList.get(currentIndex);
+		this.currentToken = this.currentLexeme.getToken();
 	}
 
 	public void analyze(){
 
 
+
 		for (int i = 0;i < currentIndex;i++)
 		{
-
-
-
-
-
+			if (currentToken)
 
 
 			advance();
@@ -49,104 +48,11 @@ public class SyntacticAnalyzer {
 	private void advance() {
 		currentIndex++;
 		currentLexeme = lexemesList.get(currentIndex);
+		currentToken = currentLexeme.getToken();
 	}
-	
-	private void program() {
-		this.decl();
-	}
-	
-	private void decl() {
-		if (exTokenType instanceof IReservedWord) {
-			
-		}
-	}
-	
-	private void variableDecl() {
-		
-	}
-	
-	private void variable() {
-		
-	}
-	
-	private void type() {
-		
-	}
-	
-	private void functionDecl() {
-		
-	}
-	
-	private void formals() {
-		
-	}
-	
-	private void classDecl() {
-		
-	}
-	
-	private void field() {
-		
-	}
-	
-	private void interfaceDecl() {
-		
-	}
-	
-	private void prototype() {
-		
-	}
-	
-	private void stmtBlock() {
-		
-	}
-	
-	private void stmt() {
-		
-	}
-	
-	private void ifStmt() {
-		
-	}
-	
-	private void whileStmt() {
-		
-	}
-	
-	private void forStmt() {
-		
-	}
-	
-	private void returnStmt() {
-		
-	}
-	
-	private void breakStmt() {
-		
-	}
-	
-	private void printStmt() {
-		
-	}
-	
-	private void expr() {
-		
-	}
-	
-	private void lValue() {
-		
-	}
-	
-	private void call() {
-		
-	}
-	
-	private void actuals() {
-		
-	}
-	
-	private void constant() {
-		
+
+	private LinkedList<Lexeme> filterLineNumber(int lineNumber){
+		return lexemesList.stream().filter(l -> l.lineNumber == lineNumber).collect(Collectors.toCollection(LinkedList::new));
 	}
 }
 
